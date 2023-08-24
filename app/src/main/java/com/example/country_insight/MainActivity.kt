960c8b7179e3e999bdef3f8b14e81de621d95818
Kanat Bayalbayev <Kanatkz07@mail.ru>
2023-08-24 14:18:31 +0600

    created Layout

--- app/src/main/java/com/example/country_insight/MainActivity.kt
+++ app/src/main/java/com/example/country_insight/MainActivity.kt
@@ -1,27 +1,63 @@
 package com.example.country_insight
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.lifecycle.Observer
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.room.util.convertUUIDToByte
-import com.example.country_insight.api.ApiFactory
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
+import com.example.country_insight.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        val countryName = "Kazakhstan"
-        viewModel.getData().observe(this, Observer {
-            Log.d("Check", it.toString())
-        })
-        viewModel.loadData(countryName)
+
+        val capitalTemplate = applicationContext.resources.getString(R.string.capital_template)
+        val subregionTemplate = applicationContext.resources.getString(R.string.subregion_template)
+        val populationTemplate =
+            applicationContext.resources.getString(R.string.population_template)
+        viewModel.getData().observe(this) {
+            for (country in it) {
+                val name = country.name
+                Picasso.get().load(country.flags.png).into(binding.imageViewFlag)
+                binding.textViewCountryName.text = name.common
+                binding.textViewCapital.text = String.format(capitalTemplate, country.capital[0])
+                binding.textViewSubregion.text = String.format(subregionTemplate, country.subregion)
+                binding.textViewPopulation.text =
+                    String.format(populationTemplate, country.population)
+                Log.d("Country", country.toString())
+            }
+            Log.d("Country", it.toString())
+
+        }
+
+        viewModel.getIsLoading().observe(this)  {
+            if (it) {
+                binding.progressBar.visibility = View.VISIBLE
+            } else {
+                binding.progressBar.visibility = View.GONE
+            }
+        }
+
+        viewModel.getIsError().observe(this)  {
+            if (it) {
+                Toast.makeText(this,"Ошибка загрузки!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+        binding.buttonToGetData.setOnClickListener {
+            val countryName = binding.inputFromUser.text.toString().trim()
+            Log.d("MainActivity", countryName)
+            viewModel.loadData(countryName)
+        }
 
 
     }
